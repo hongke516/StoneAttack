09a2f0a23057971508bd376cd4131e629869a82c
Previrtu <previrtu@isdev.kr>
2013-06-26 17:22:59 +0900

    MeteoSprite Update 부분 구현

--- StoneAttack/src/MeteoSprite.js
+++ StoneAttack/src/MeteoSprite.js
@@ -59,6 +59,33 @@ var MeteoSprite = cc.Sprite.extend({
 	},
 
 	update: function() {
+		this.currentAngle += 3;
+		if(this.currentAngle >= 360)
+			this.currentAngle -= 360;
 
+		this.setRotation(this.currentAngle);
+
+		this.meteoY += this.dropSpeed;
+		var dx = this.meteoX - gameData.playerX,
+			x  = gameData.planetCenterX + Math.cos(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.meteoY,
+			y  = gameData.planetCenterY + Math.sin(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.meteoY;
+
+		this.setPosition(cc.p(x, y));
+		if(this.meteoY < 163) {
+			gameData.gameScene.collidePlanet(this);
+		} else if(gameData.gameScene.isShield && Math.pow(gameData.shieldX - x, 2) + Math.pow(gameData.shieldY - y, 2) < 1600) {
+			if(!(this.isShield || this.isHp)) {
+				gameData.gameScene.collideShield(this);
+			} else {
+				gameData.gameScene.collideItem(this);
+			}
+		} else if(x > gameData.playerRectX1 && x < gameData.playerRectX2 &&
+				  y > gameData.playerRectY1 && y < gameData.playerRectY2) {
+			if(!(this.isShield || this.isHp)) {
+				gameData.gameScene.collidePlayer(this);
+			} else {
+				gameData.gameScene.collideItem(this);
+			}
+		}
 	}
 });
