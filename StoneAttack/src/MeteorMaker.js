d299f90c607f6e761dd721454cca840a86a1c75c
Previrtu <previrtu@isdev.kr>
2013-06-26 16:45:14 +0900

    MeteorMaker Class 추가

--- StoneAttack/src/MeteorMaker.js
+++ StoneAttack/src/MeteorMaker.js
@@ -1,6 +1,37 @@
-// var MeteorMaker = cc.Class.extend
-// (
-	// {
-// 		
-	// }
-// )
+var MeteorMaker = cc.Class.extend({
+	// public
+	currentX: 0,
+	isRightDirection: true,
+	makeCoolTime: 25,
+
+	// private
+	_currentCoolTime: 0,
+
+	update: function() {
+		this.isRightDirection = Math.floor(Math.random() * 100 % 5) == 0 ? !this.isRightDirection : isRightDirection;
+
+		this.currentX += this.isRightDirection ? 10 : -10;
+		this.currentX += this.currentX >= 360 ? -360 : ( this.currentX < 0 ? 360 : 0 );
+
+		this._currentCoolTime++;
+		if(this.makeCoolTime <= this._currentCoolTime) {
+			this._currentCoolTime = 0;
+			this.newInstance(this.currentX);
+		}
+	},
+
+	newInstance: function(currentX) {
+		// GameScene::gameScene->makeMeteor(currentX);
+	},
+
+	create: function(x) {
+		var pobInstance = new MeteorMaker(x ? x : Math.floor(Math.random() * 1000 % 360));
+		if(pobInstance) {
+			pobInstance.autoRelease();
+			return pobInstance;
+		}
+
+		delete pobInstance;
+		return null;
+	}
+});
