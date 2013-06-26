83e428b8cbf67e174e212536a7a8a24aae0184cf
Previrtu <previrtu@isdev.kr>
2013-06-26 17:59:41 +0900

    localStorage 기반 GameData 저장/로드기능 추가

--- StoneAttack/src/gameCache.js
+++ StoneAttack/src/gameCache.js
@@ -7,18 +7,42 @@
 	}
 
 	var Cache = {
-		get: function(key) {
+		_get: function(key) {
 			if(!localStorage.hasOwnProperty(key)) return null;
 			return localStorage.getItem(key);
 		},
 
-		set: function(key, value) {
+		_set: function(key, value) {
 			localStorage.setItem(key, value);
 		},
 
-		remove: function(key) {
+		_remove: function(key) {
 			if(!localStorage.hasOwnProperty(key)) return null;
 			localStorage.removeItem(key);
+		},
+
+		save: function() {
+			var arr = [];
+			arr.push( GameData.isMusicSound ? 1 : 0 );
+			arr.push( GameData.isSoundEffect ? 1 : 0 );
+			arr.push( GameData.HighScore.Normal );
+			arr.push( GameData.HighScore.Hardcore );
+			arr.push( GameData.HighScore.SavedVersion );
+			arr.push( GameData.currentBgmNumber );
+
+			this._set('__STONEATTACK_DATA', arr.join(','));
+		},
+
+		load: function() {
+			if(this._get('__STONEATTACK_DATA') == null) return;
+			var arr = this._get('__STONEATTACK_DATA').split(',');
+
+			GameData.isMusicSound = arr[0] == 1 ? true : false;
+			GameData.isSoundEffect = arr[1] == 1 ? true : false;
+			GameData.HighScore.Normal = arr[2];
+			GameData.HighScore.HardCore = arr[3];
+			GameData.HighScore.SavedVersion = arr[4];
+			GameData.currentBgmNumber = arr[5];
 		}
 	};
 
