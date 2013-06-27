4f554ff5d35044de520533b231bc6f77d4e3ac9a
Previrtu <previrtu@isdev.kr>
2013-06-27 09:32:32 +0900

    MeteorSprite 변경

--- StoneAttack/src/MeteorSprite.js
+++ StoneAttack/src/MeteorSprite.js
@@ -1,92 +1,93 @@
-var MeteorSprite = cc.Sprite.extend({
-	_currentAngle: 0,
+var MeteorSprite = {
+	items: [],
 
-	// static
-	minDropSpeed: 1,
-	maxDropSpeed: 4,
-
-	// public
-	isShield: false,
-	isHp: false,
-
-	dropSpeed: 0,
-	MeteorX: Math.floor(Math.random() * 1000 % 360), MeteorY: 680,
-
-	construct: function(x) {
-		// this.dropSpeed = Math.floor(Math.random() * 1000) % (this.maxDropSpeed - this.minDropSpeed) + this.minDropSpeed;
-		this.dropSpeed = Math.floor(Math.random() * (this.maxDropSpeed - this.minDropSpeed)) + this.minDropSpeed;		
-		// if(x) {
-			this.MeteorX = x;
-		// }
+	properties: {
+		minDropSpeed: 1,
+		maxDropSpeed: 4
 	},
 
 	create: function(x) {
-		var pobSprite = new MeteorSprite();
-		pobSprite.construct(x);
-		var randNum = Math.floor(Math.random() * 4);
-		if(pobSprite && pobSprite.initWithFile(randNum == 0 ? s_metheo1_png : randNum == 1 ? s_metheo2_png : randNum == 2 ? s_metheo3_png : s_metheo4_png)) {
-			return pobSprite;
-		}
+		var obj = {
+			currentAngle: 0,
+			isShield: false,
+			isHp: false,
 
-		delete pobSprite;
-		return null;
+			dropSpeed: Math.floor(Math.random() * (this.maxDropSpeed - this.minDropSpeed)) + this.minDropSpeed,
+			MeteorX: x ? x : Math.floor(Math.random() * 360),
+			sprite: null
+		};
+
+		var randNum = Math.floor(Math.random() * 4);
+		var img = pobSprite.initWithFile(randNum == 0 ? s_metheo1_png : randNum == 1 ? s_metheo2_png : randNum == 2 ? s_metheo3_png : s_metheo4_png));
+		var sprite = cc.Sprite.create(img);
+
+		this.items.push(obj);
 	},
 
 	createShield: function(x) {
-		var pobSprite = new MeteorSprite();
-		pobSprite.construct(x);
-		if(pobSprite && pobSprite.initWithFile(s_shield_png)) {
-			pobSprite.isShield = true;
-			pobSprite.dropSpeed = 4;
-			return pobSprite;
-		}
+		var obj = {
+			currentAngle: 0,
+			isShield: true,
+			isHp: false,
 
-		delete pobSprite;
-		return null;
+			dropSpeed: 4,
+			MeteorX: x ? x : Math.floor(Math.random() * 360),
+			sprite: null
+		};
+
+		var randNum = Math.floor(Math.random() * 4);
+		var sprite = cc.Sprite.create(s_shield_png);
+
+		this.items.push(obj);
 	},
 
-	createHp: function() {
-		var pobSprite = new MeteorSprite();
-		pobSprite.construct();
-		if(pobSprite && pobSprite.initWithFile(s_life_png)) {
-			pobSprite.isHp = true;
-			pobSprite.dropSpeed = 4;
-			return pobSprite;
-		}
+	createHp: function(x) {
+		var obj = {
+			currentAngle: 0,
+			isShield: false,
+			isHp: true,
 
-		delete pobSprite;
-		console.log('FD');
-		return null;
+			dropSpeed: 4,
+			MeteorX: x ? x : Math.floor(Math.random() * 360),
+			sprite: null
+		};
+
+		var randNum = Math.floor(Math.random() * 4);
+		var sprite = cc.Sprite.create(s_life_png);
+
+		this.items.push(obj);
 	},
 
 	update: function() {
-		this.currentAngle += 3;
-		if(this.currentAngle >= 360)
-			this.currentAngle -= 360;
+		for(var i = 0; i < this.items.length; i++) {
+			this.items[i].currentAngle += 3;
+			if(this.items[i].currentAngle >= 360)
+				this.items[i].currentAngle -= 360;
 
-		this.setRotation(this.currentAngle);
+			this.items[i].sprite.setRotation(this.items[i].currentAngle);
 
-		this.MeteorY += this.dropSpeed;
-		var dx = this.MeteorX - GameData.playerX,
-			x  = GameData.planetCenterX + Math.cos(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.MeteorY,
-			y  = GameData.planetCenterY + Math.sin(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.MeteorY;
+			this.items[i].MeteorY += this.items[i].dropSpeed;
+			var dx = this.items[i].MeteorX - GameData.playerX,
+			x  = GameData.planetCenterX + Math.cos(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.items[i].MeteorY,
+			y  = GameData.planetCenterY + Math.sin(Math.PI / 180 * (dx < 0 ? dx + 360 : dx)) * this.items[i].MeteorY;
 
-		this.setPosition(cc.p(x, y));
-		if(this.MeteorY < 163) {
-			GameData.gameScene.collidePlanet(this);
-		} else if(GameData.gameScene.isShield && Math.pow(GameData.shieldX - x, 2) + Math.pow(GameData.shieldY - y, 2) < 1600) {
-			if(!(this.isShield || this.isHp)) {
-				GameData.gameScene.collideShield(this);
-			} else {
-				GameData.gameScene.collideItem(this);
-			}
-		} else if(x > GameData.playerRectX1 && x < GameData.playerRectX2 &&
-				  y > GameData.playerRectY1 && y < GameData.playerRectY2) {
-			if(!(this.isShield || this.isHp)) {
-				GameData.gameScene.collidePlayer(this);
-			} else {
-				GameData.gameScene.collideItem(this);
+			this.items[i].sprite.setPosition(cc.p(x, y));
+			if(this.items[i].MeteorY < 163) {
+				GameData.gameScene.collidePlanet(this);
+			} else if(GameData.gameScene.isShield && Math.pow(GameData.shieldX - x, 2) + Math.pow(GameData.shieldY - y, 2) < 1600) {
+				if(!(this.items[i].isShield || this.items[i].isHp)) {
+					GameData.gameScene.collideShield(this);
+				} else {
+					GameData.gameScene.collideItem(this);
+				}
+			} else if(x > GameData.playerRectX1 && x < GameData.playerRectX2 &&
+				y > GameData.playerRectY1 && y < GameData.playerRectY2) {
+				if(!(this.items[i].isShield || this.items[i].isHp)) {
+					GameData.gameScene.collidePlayer(this);
+				} else {
+					GameData.gameScene.collideItem(this);
+				}
 			}
 		}
 	}
-});
+};
