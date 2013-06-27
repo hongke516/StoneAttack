e9040df059204ad071bdafd29deb31d1246b0697
Previrtu <previrtu@isdev.kr>
2013-06-27 10:32:22 +0900

    ParticleExplosionSprite 수정

--- StoneAttack/src/ParticleExplosionSprite.js
+++ StoneAttack/src/ParticleExplosionSprite.js
@@ -1,59 +1,45 @@
-var ParticleExplosionSprite = cc.Sprite.extend({
-	// private
-	fileName: '',
-	vectorX: 0, vectorY: 0,
-	opacityTime: 0, deltaOpacity: 0,
-	currentAngle: 0,
-
-	// Constructor
-	construct: function(point) { // point = cc.Point (cc.p)
-		this.vectorX = Math.round(Math.floor(Math.random() * 1000 % 100) - 50 / 50);
-		this.vectorY = Math.round(Math.floor(Math.random() * 1000 % 100) - 50 / 50);
-		this.currentAngle = Math.floor(Math.random() * 1000 % 360);
-		this.opacityTime = 150;
-		this.deltaOpacity = 8;
-
-		this.setPosition(point);
-		this.setRotation(this.currentAngle);
-		this.setVisible(false);
-	},
+var ParticleExplosionSprite = {
+	items: [],
 
 	createWithPoint: function(fileName, point) {
-		var pSprite = new ParticleExplosionSprite();
-		pSprite.construct();
-		if(pSprite && pSprite.initWithFile(fileName)) {
-			return pSprite;
-		}
+		var obj = {
+			fileName: '',
+			vectorX: Math.round(Math.floor(Math.random() *  100) - 50 / 50), 
+			vectorY: Math.round(Math.floor(Math.random() *  100) - 50 / 50),
+			opacityTime: 150, deltaOpacity: 8,
+			currentAngle: Math.floor(Math.random() * 360),
+			sprite: null
+		};
 
-		delete pSprite;
-		return null;
+		var sprite = cc.Sprite.create(fileName);
+		sprite.setPosition(point);
+		sprite.setRotation(obj.currentAngle);
+		sprite.setVisible(false);
 	},
 
 	updatePoint: function() {
 		if(GameData.gameState == GameData.STATE_PAUSE) return;
-		
-		this.opacityTime -= this.deltaOpacity;
-		if(this.opacityTime <= 0) {
-        	//unschedule(schedule_selector(ParticleExplosionSprite::updatePoint));
-			this.unschedule(this.updatePoint);
-			this.removeFromParentAndCleanup(true);
-			return;
+
+		for(var i = 0; i < this.items.length; i++) {
+			this.items[i].opacityTime -= this.items[i].deltaOpacity;
+			if(this.items[i].opacityTime <= 0) {
+				this.items[i].sprite.removeFromParentAndCleanup(true);
+				return;
+			}
+
+			this.items[i].sprite.setOpacity(this.items[i].opacityTime);
+			this.items[i].sprite.setPosition(cc.p(this.items[i].sprite.getPositionX() + this.items[i].vectorX, this.items[i].sprite.getPositionY() + this.items[i].vectorY));
+			this.items[i].currentAngle += 5;
+			if(this.items[i].currentAngle >= 360) this.items[i].currentAngle -= 360;
+			this.items[i].sprite.setRotation(this.items[i].currentAngle);
 		}
-
-		this.setOpacity(this.opacityTime);
-		this.setPosition(cc.p(this.getPositionX() + this.vectorX, this.getPositionY() + this.vectorY));
-		this.currentAngle += 5;
-		if(this.currentAngle >= 360) this.currentAngle -= 360;
-		this.setRotation(this.currentAngle);
-	},
-
-	setDeltaOpacity: function(deltaOpacity) {
-		this.deltaOpacity = deltaOpacity;
 	},
 
 	start: function() {
-		this.setVisible(true);
-		this.setOpacity(this.opacityTime);
-		this.schedule(this.updatePoint);
+		var i = this.items.length - 1;
+
+		this.items[i].sprite.setVisible(true);
+		this.items[i].sprite.setOpacity(this.items[i].opacityTime);
+		// this.schedule(this.updatePoint);
 	}
-});
+};
